6349867a1e2035012b42e46a9bd163a8a5338a39
John Lester Villarde <142194835+JohnLesterVillarde@users.noreply.github.com>
2024-06-30 11:38:48 +0800

    feat: add election page to set election event

--- dist/utils/types/ElectionLog.js
+++ dist/utils/types/ElectionLog.js
@@ -1,2 +1,3 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiRWxlY3Rpb25Mb2cuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi9zcmMvdXRpbHMvdHlwZXMvRWxlY3Rpb25Mb2cudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiJ9
--- dist/web/index.js
+++ dist/web/index.js
@@ -10,3 +10,4 @@ const router = (0, express_1.Router)();
 router.use(generalAccess_1.default);
 router.use("/admin", admin_1.default);
 exports.default = router;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvd2ViL2luZGV4LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUEscUNBQThCO0FBQzlCLDJFQUFrRDtBQUNsRCwyREFBd0M7QUFFeEMsTUFBTSxNQUFNLEdBQUcsSUFBQSxnQkFBTSxHQUFFLENBQUE7QUFFdkIsTUFBTSxDQUFDLEdBQUcsQ0FBQyx1QkFBYSxDQUFDLENBQUE7QUFDekIsTUFBTSxDQUFDLEdBQUcsQ0FBQyxRQUFRLEVBQUUsZUFBVyxDQUFDLENBQUE7QUFFakMsa0JBQWUsTUFBTSxDQUFBIn0=
