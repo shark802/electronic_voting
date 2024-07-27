2de208afdac108b9aebacbdaa9302629b32f0a9e
John Lester Villarde <142194835+JohnLesterVillarde@users.noreply.github.com>
2024-07-27 15:06:46 +0800

    feat: add toggle functionality for more option of candidate management

--- public/javascript/admin/candidate_manage.js
+++ public/javascript/admin/candidate_manage.js
@@ -1,3 +1,4 @@
+// import { setTimeout } from "timers/promises";
 import "/javascript/logout.js";
 
 const candidate_nav = document.querySelector("#candidate_nav");
@@ -18,6 +19,7 @@ document.querySelector("#show-sidebar").addEventListener("click", () => {
 document.querySelector("#hide-sidebar").addEventListener('click', () => {
     $("#sidebar").hide(100);
 });
+
 
 document.querySelectorAll("#election").forEach(election => {
     election.addEventListener('click', event => {
@@ -46,6 +48,27 @@ document.querySelectorAll("#position").forEach(position => {
         displayFetchCandidate(candidates);
     });
 });
+
+document.querySelectorAll("#candidates-section").forEach(section => { // Open more option
+    section.addEventListener('click', (event) => {
+        if (event.target.closest('#option-section')) {
+            toggleMoreOptionDisplay(event);
+        }
+
+        if (event.target.closest("#edit")) {
+            console.log("EDIT");
+        }
+
+    })
+});
+
+document.addEventListener('click', event => { // Close if there is an open section  
+    if (!event.target.closest("#option-section")) {
+        document.querySelectorAll("#candidates-section").forEach(candidateSection => {
+            candidateSection.querySelectorAll("#more-option").forEach(section => $(section).slideUp(100))
+        })
+    }
+})
 
 // change the the position selected if click
 function styleSelectedPosition(event) {
@@ -105,9 +128,46 @@ function displayFetchCandidate(candidates) {
                 <td class="text-xs font-medium py-2 pl-2 text-gray-600 border-b-gray-400">${candidate.course}</td>
                 <td class="text-xs font-medium py-2 pl-2 text-gray-600 border-b-gray-400">${candidateAddedAt}</td>
                 <td class="text-xs font-medium py-2 pl-2 text-gray-600 border-b-gray-400 text-center">${status}</td>
-                <td> <div class="flex justify-center gap-4 items-center"> <img src="/img/more.webp" class="w-5 hover:cursor-pointer opacity-70 hover:rounded-full hover:bg-blue-300"/> <p class="font-semibold hover:cursor-pointer text-blue-500">Edit</p> </div> </td>
+                <td>
+                    <div class="flex justify-center gap-4 items-center">
+                        <div id="option-section" class="relative">
+                            <img src="/img/more.webp" class="w-5 hover:cursor-pointer opacity-70 hover:rounded-full hover:bg-blue-300"/>
+                            <div id="more-option" class="absolute z-10 right-0 gap-2 px-1 py-3 hidden bg-white border border-solid rounded shadow-md w-36 h-fit top-7">
+
+                                <div id="toggleElectionEventVisibility" class="flex items-center px-3 my-2 transition-all rounded-sm hover:bg-blue-200">
+                                    <img id="viewStatusImage" src="/img/hide.webp" alt="hide" class="w-4 h-4">
+                                    <p id="viewStatus" class="ml-2">Deactivate</p>
+                                </div>
+
+                                <div id="delete_election_button" class="flex items-center px-3 my-2 transition-all rounded-sm hover:bg-blue-200">
+                                    <img src="/img/trash.webp" alt="delete" class="w-4 h-4">
+                                    <p class="ml-2">Delete</p>
+                                </div>
+
+                            </div>
+                        </div> 
+                        <p id="edit" class="font-semibold hover:cursor-pointer text-blue-500">Edit</p>
+                    </div> 
+                </td>
             </tr>
         `
         document.querySelector(`tbody[data-election-id="${candidate.election_id}"]`).innerHTML += tableRow;
     })
 }
+
+function toggleMoreOptionDisplay(event) {
+
+    document.querySelectorAll("#candidates-section").forEach(section => { // Close other more option if there is opened before opening new option
+        section.querySelectorAll("#more-option").forEach(option => {
+            if (option !== event.target.closest("#option-section").querySelector("#more-option")) {
+                $(option).slideUp(100);
+            }
+        })
+    });
+
+    if (event.target.closest("#more-option")) return;
+    $(event.target.closest("#option-section").querySelector("#more-option")).slideToggle(300)
+}
+
+
+
