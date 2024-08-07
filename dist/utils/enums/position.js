5ae004ef59da60e9a407feaad31e85572258fb86
John Lester Villarde <142194835+JohnLesterVillarde@users.noreply.github.com>
2024-08-07 19:36:59 +0800

    feat: add register device form

--- dist/utils/enums/position.js
+++ dist/utils/enums/position.js
@@ -9,3 +9,4 @@ var Position;
     Position["PIO"] = "PIO";
     Position["Treasurer"] = "TREASURER";
 })(Position || (exports.Position = Position = {}));
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicG9zaXRpb24uanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi9zcmMvdXRpbHMvZW51bXMvcG9zaXRpb24udHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBQUEsSUFBWSxRQU1YO0FBTkQsV0FBWSxRQUFRO0lBQ2hCLG1DQUF1QixDQUFBO0lBQ3ZCLDZDQUFpQyxDQUFBO0lBQ2pDLCtCQUFtQixDQUFBO0lBQ25CLHVCQUFXLENBQUE7SUFDWCxtQ0FBdUIsQ0FBQTtBQUMzQixDQUFDLEVBTlcsUUFBUSx3QkFBUixRQUFRLFFBTW5CIn0=
--- public/javascript/landing-page.js
+++ public/javascript/landing-page.js
@@ -1,4 +1,5 @@
 import { displayRedirectMessage } from "/javascript/helper/showRedirectMessage.js";
+import { isValidText } from "/javascript/formInputValidator/isValidText.js"
 import "/javascript/landing-page-login.js";
 
 const loginModal = document.querySelector('#login-modal');
@@ -12,3 +13,32 @@ document.querySelector("#login-modal-exit").addEventListener('click', function (
 });
 
 displayRedirectMessage();
+openRegisterDeviceModal();
+closeRegisterDeviceModal();
+submitRegisterDeviceForm();
+
+function openRegisterDeviceModal() {
+    document.querySelector('#register-device-button').addEventListener('click', () => {
+        document.querySelector('#registerDeviceModal').showModal();
+    })
+}
+
+function closeRegisterDeviceModal() {
+    document.querySelector('#closeRegisterDevice').addEventListener('click', () => {
+        document.querySelector('#registerDeviceModal').close();
+    })
+}
+
+function submitRegisterDeviceForm() {
+    document.querySelector('#registerDeviceForm').addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const codeName = document.querySelector('#code-name');
+        const codeNameErrorMessage = document.querySelector('#codeNameErrorMessage');
+
+        if (!isValidText([codeName], codeNameErrorMessage)) return;
+
+        console.log('valid');
+
+    });
+}
