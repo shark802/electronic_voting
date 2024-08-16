38dc3f27ec9f34dfe14e8006eae8599b20bdf641
John Lester Villarde <142194835+JohnLesterVillarde@users.noreply.github.com>
2024-08-16 21:43:58 +0800

    feat: implement api to update election population/voter

--- public/javascript/admin/dashboard_overview.js
+++ public/javascript/admin/dashboard_overview.js
@@ -1,3 +1,4 @@
+import { confirmErrorAlert, confirmAlert, showSwalSuccessToast } from "/javascript/helper/sweetAlertFunctions.js"
 import "/javascript/logout.js"
 
 const dashboard_nav = document.querySelector("#dashboard_nav")
@@ -20,22 +21,59 @@ document.querySelector("#hide-sidebar").addEventListener('click', () => {
     $("#sidebar").hide(100);
 });
 
-// function toggleElectionOverview(event) {
-//     if (!event.target.closest('#program')) $(event.target.closest('#program')).slideToggle(300);
-// }
 
 
+
+// Event listener for opening update population form
 document.querySelector('#overview-container').addEventListener('click', (event) => {
     if (event.target.id !== 'manage') return;
+
+    document.querySelector('#manage-population-modal').showModal();
 
     displayPopulationInput(event);
     calculateTotalVoterOnInputEvent()
 
+});
+
+// Event listener for submitting update population form
+document.querySelector('#manage-election-population').addEventListener('submit', async (event) => {
+    event.preventDefault();
+    try {
+        if (Object.entries(getInputPopulationPerProgram()).length < 1) return; // return when nothing inputted.
+
+        document.querySelector('#manage-population-modal').close();
+        const action = await confirmAlert('Confirm submission');
+        if (!action.isConfirmed) return document.querySelector('#manage-population-modal').showModal(); // return if not confirmed submission.
+
+        const response = await submitUpdateVoterPopulationForm();
+        const responseObject = await response.json();
+
+        if (!response.ok) return confirmErrorAlert(responseObject.message);
+
+        showSwalSuccessToast(responseObject.message);
+
+
+        // update election overview display
+
+        // const electionId = event.target.querySelector('#hidden-election-id').value;
+        // console.log(electionId);
+
+
+    } catch (error) {
+        console.error(error);
+    }
+
 })
+
+// Event listener for closing the modal
+document.querySelector('#manage-population-modal-exit').addEventListener('click', (event) => event.target.closest('dialog').close())
+
 
 function displayPopulationInput(event) {
     const electionId = event.target.closest('section').querySelector('#election-id').textContent;
     const programList = event.target.closest('section').querySelectorAll('#program');
+
+    document.querySelector('#manage-election-population').querySelector('#hidden-election-id').value = electionId;
 
     const input = Array.from(programList).map(element => {
         const programCode = element.querySelector('#program-code').textContent.trim();
@@ -54,21 +92,6 @@ function displayPopulationInput(event) {
 
 }
 
-// function calculateTotalVoterOnInputEvent() {
-//     const form = document.querySelector('#manage-election-population');
-
-//     form.querySelectorAll('input[type="number"]').forEach(input => {
-//         input.addEventListener('input', () => {
-//             const total = Array.from(form.querySelectorAll('input[type="number"]'))
-//                 .reduce((total, currentInput) => total + Number(currentInput.value), 0);
-//             console.log(total);
-
-//             form.querySelector('#total-voter').value = total;
-//         });
-//     });
-// }
-
-
 function calculateTotalVoterOnInputEvent() {
     const form = document.querySelector('#manage-election-population');
 
@@ -76,10 +99,43 @@ function calculateTotalVoterOnInputEvent() {
         input.addEventListener('input', () => {
             const total = Array.from(form.querySelectorAll('input[type="number"]')).reduce((total, currentInput) => total + Number(currentInput.value), 0);
 
-            // console.log(total);
             form.querySelector('#total-voter').value = total;
         });
     });
+}
+
+async function submitUpdateVoterPopulationForm() {
+    const populationPerProgram = getInputPopulationPerProgram();
+    const total = document.querySelector('#total-voter').value.trim();
+    const electionId = document.querySelector('#hidden-election-id').value.trim();
+
+    const response = await fetch(`/api/population/${electionId}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ total, populationPerProgram })
+    });
+
+    return response;
+
+}
+
+function getInputPopulationPerProgram() {
+    const populationPerProgramObject = Array.from(document.querySelector('#input-container').querySelectorAll('input[type=number')).reduce((object, currentInput) => {
+        const programCode = currentInput.id.trim();
+        const programPopulation = currentInput.value.trim();
+
+        if (programPopulation) object[programCode] = programPopulation;
+
+        return object;
+    }, {})
+
+    return populationPerProgramObject;
+}
+
+function updateElectionOverviewDisplay(electionId, total, populationPerProgramObject) {
+    console.log(electionId);
+    console.log(total);
+    console.log(populationPerProgramObject);
 }
 
 
--- src/api/index.ts
+++ src/api/index.ts
@@ -2,8 +2,9 @@ import { Router } from "express";
 import electionRouter from "./routes/election";
 import authRouter from "./routes/auth";
 import candidateRouter from "./routes/candidate";
-import voteRouter from "./routes/vote"
-import registerDeviceRouter from "./routes/registerDevice"
+import voteRouter from "./routes/vote";
+import registerDeviceRouter from "./routes/registerDevice";
+import populationRouter from "./routes/population";
 
 const router = Router();
 
@@ -12,5 +13,6 @@ router.use(authRouter);
 router.use(candidateRouter);
 router.use(voteRouter);
 router.use(registerDeviceRouter);
+router.use(populationRouter);
 
 export default router;
