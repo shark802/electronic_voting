9168f345659e9d6e96b98511fe6d5898bdea2f1b
John Lester Villarde <142194835+JohnLesterVillarde@users.noreply.github.com>
2024-11-01 15:33:13 +0800

    fix: properly display the total voted on corresponding department in complete election dashboard

--- public/javascript/admin/complete_election_report.js
+++ public/javascript/admin/complete_election_report.js
@@ -8,13 +8,13 @@ const totalNotVotedPercentage = document.querySelector('#total-not-voted-percent
 const urlParams = `election_id=${electionId.textContent}`;
 
 async function fetchElectionTotalVoted(urlParams) { // return the total number of voted in election 
-try {
+    try {
         const url = `/api/election-voted?${urlParams}` // response array of objects {election_id, voted}. voted property contains the total number of voted in election 
         const response = await fetch(url);
         const responseObject = await response.json()
 
         if (!response.ok) return showSwalErrorToast(responseObject.message);
-        
+
         return responseObject.elections[0]?.voted ?? 0;
     } catch (error) {
         console.error(error);
@@ -57,7 +57,7 @@ document.querySelectorAll('#program').forEach(department => {
     const totalVoter = Number(departmentsTotalPopulation) || 0; // in case the population is not covertable to number like value is 'N/A', use default value 0
     const totalVoted = programTotalVoteCount[departmentCode] || 0; // in case the total number of votes is not found, use default value 0
     const totalNotVoted = totalVoter - totalVoted;
-        
-    document.querySelector(`#departmentTotalVotes`).textContent =  totalVoted; // display the total number of votes for each department
-    document.querySelector(`#departmentNumberOfNotVoted`).textContent = totalNotVoted; // display the total number of not voted
+
+    department.querySelector(`#departmentTotalVotes`).textContent = totalVoted; // display the total number of votes for each department
+    department.querySelector(`#departmentNumberOfNotVoted`).textContent = totalNotVoted; // display the total number of not voted
 });
