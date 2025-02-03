90dccdccadd858e2ef4f351d6aa4ed14a6a12b93
Jason de los Reyes <jasondelosreyes914@gmail.com>
2025-02-03 21:36:52 +0800

    update database.ts

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,13 +1,22 @@
 import mysql2 from "mysql2/promise";
 import dotenv from "dotenv";
+import url from "url"; 
 
 dotenv.config();
 
+const jawsDbUrl = process.env.JAWSDB_URL;
+if (!jawsDbUrl) {
+    throw new Error('JAWSDB_URL environment variable is not set.');
+}
+
+// Parse the JAWSDB_URL to extract connection details
+const dbUrl = new url.URL(jawsDbUrl);
+
 const pool = mysql2.createPool({
-	host: process.env.HOST,
-	user: process.env.USER,
-	password: process.env.PASSWORD,
-	database: process.env.DATABASE,
+	host: dbUrl.hostname,        // Hostname from the parsed URL
+    user: dbUrl.username,        // Username from the parsed URL
+    password: dbUrl.password,    // Password from the parsed URL
+    database: dbUrl.pathname.slice(1),
 	waitForConnections: true,
 	connectionLimit: 10,
 	queueLimit: 0,
