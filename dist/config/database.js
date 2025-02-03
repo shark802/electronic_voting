5c43653fec05c6488dd75f46713c12f115f09dce
Jason de los Reyes <jasondelosreyes914@gmail.com>
2025-02-03 20:48:54 +0800

    update JAWSDB configuration

--- dist/config/database.js
+++ dist/config/database.js
@@ -6,14 +6,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.pool = void 0;
 const promise_1 = __importDefault(require("mysql2/promise"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const url_1 = __importDefault(require("url"));
+
 dotenv_1.default.config();
+
+// Parse the JAWSDB_URL from Heroku's config vars
+const dbUrl = new url_1.default.URL(process.env.JAWSDB_URL);
+
 const pool = promise_1.default.createPool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
+    host: dbUrl.hostname,      // Hostname from the parsed URL
+    user: dbUrl.username,      // Username from the parsed URL
+    password: dbUrl.password,  // Password from the parsed URL
+    database: dbUrl.pathname.slice(1),  // Remove the leading "/" from the database name
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
+    ssl: { rejectUnauthorized: false }, // Add SSL configuration for Heroku/JawsDB
 });
+
 exports.pool = pool;
