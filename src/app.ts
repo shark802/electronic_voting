ca0aabfd53e8cb25c9f4c2751460adc7527e85f2
Jason de los Reyes <jasondelosreyes914@gmail.com>
2025-02-05 22:14:43 +0800

    revert changes

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,6 @@ import apiRoutes from "./api";
 import webRoutes from "./web";
 import expressMysqlSession from "express-mysql-session";
 import upload from './config/multerConfig';
-import url from "url"; 
 
 // register all files that listening on event emitter
 import './events';
@@ -28,29 +27,12 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../views"));
 app.use(express.static(path.join(__dirname, "../public")));
 
-// Parse the JAWSDB_URL from Heroku's config vars
-const jawsDbUrl = process.env.JAWSDB_URL;
-
-if (!jawsDbUrl) {
-    throw new Error('JAWSDB_URL environment variable is not set.');
-}
-
-// Parse the JAWSDB_URL from Heroku's config vars
-const dbUrl = new url.URL(jawsDbUrl);
-
-// Log the URL parts for debugging purposes
-console.log('DB URL:', dbUrl);
-console.log('Host:', dbUrl.hostname);
-console.log('User:', dbUrl.username);
-console.log('Password:', dbUrl.password);
-console.log('Database:', dbUrl.pathname.slice(1));
-
 const MySQLStore = expressMysqlSession(session);
 const sessionStore = new MySQLStore({
-    host: dbUrl.hostname,
-    user: dbUrl.username,
-    password: dbUrl.password,
-    database: dbUrl.pathname.slice(1),
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
     clearExpired: true,
     expiration: 60 * 60000,
     createDatabaseTable: true,
--- src/config/database.ts
+++ src/config/database.ts
@@ -1,29 +1,13 @@
 import mysql2 from "mysql2/promise";
 import dotenv from "dotenv";
-import url from "url"; 
 
 dotenv.config();
 
-const jawsDbUrl = process.env.JAWSDB_URL;
-if (!jawsDbUrl) {
-    throw new Error('JAWSDB_URL environment variable is not set.');
-}
-
-// Parse the JAWSDB_URL to extract connection details
-const dbUrl = new url.URL(jawsDbUrl);
-
-// Log the URL parts for debugging purposes
-console.log('DB URL:', dbUrl);
-console.log('Host:', dbUrl.hostname);
-console.log('User:', dbUrl.username);
-console.log('Password:', dbUrl.password);
-console.log('Database:', dbUrl.pathname.slice(1));
-
 const pool = mysql2.createPool({
-	host: dbUrl.hostname,        // Hostname from the parsed URL
-    user: dbUrl.username,        // Username from the parsed URL
-    password: dbUrl.password,    // Password from the parsed URL
-    database: dbUrl.pathname.slice(1),
+	host: process.env.HOST,
+	user: process.env.USER,
+	password: process.env.PASSWORD,
+	database: process.env.DATABASE,
 	waitForConnections: true,
 	connectionLimit: 10,
 	queueLimit: 0,
